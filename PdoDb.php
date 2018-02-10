15fc2ddae807fe65042db267c74247aba8c387b5
Channy <Chan-uon@users.noreply.github.com>
2018-02-10 17:48:01 -0500

    Add db.ini to store database credentials Modify PdoDb.class to read credentials from db.ini

--- PdoDb.php
+++ PdoDb.php
@@ -10,8 +10,10 @@ class PdoDb{
 
     public static function getInstance(){
         if(self::$instance == null){
+            $db = parse_ini_file('db.ini');
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$instance = new PDO('mysql:host=localhost;dbname=test','root','',$pdo_options);
+            self::$instance = new PDO('mysql:host='.$db['host'].';dbname='.$db["database"],
+                                    $db['username'], $db['password'], $pdo_options);
         }
         return self::$instance;
     }
