6297cc61b16677463269187c006d3ba2c06c1f10
Channy <Chan-uon@users.noreply.github.com>
2018-02-10 17:25:17 -0500

    Modify getInstance() to return an instance of PDO

--- PdoDb.php
+++ PdoDb.php
@@ -3,17 +3,15 @@
 * Singleton Database Class for PDO connection
 */
 class PdoDb{
-    private $db;
     private static $instance = null;
 
     private function __construct() {
-        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        $this->db = new PDO('mysql:host=;dbname=','root','',$pdo_options);
     }
 
     public static function getInstance(){
         if(self::$instance == null){
-            self::$instance = PdoDb();
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            self::$instance = new PDO('mysql:host=localhost;dbname=test','root','',$pdo_options);
         }
         return self::$instance;
     }
