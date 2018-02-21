7a6760d526d50b783ff0c821001f0d5daf255b38
Channy <Chan-uon@users.noreply.github.com>
2018-02-20 23:22:05 -0500

    Add comments to methods in Controller's children class

--- controllers/Controller.class.php
+++ controllers/Controller.class.php
@@ -12,6 +12,14 @@ class Controller{
     private function __construct() {
     }
 
+    /**
+    * Method view($view_name, $data = []).
+    * Include the view using the $view_name.
+    *
+    * @param string $view_name The name of the view.
+    * @param array $data The data being passed to the view.
+    * @return void
+    */
     public function view($view_name, $data = []) {
         require_once("./views/{$view_name}");
     }
--- controllers/ControllerHome.class.php
+++ controllers/ControllerHome.class.php
@@ -6,6 +6,16 @@ namespace MyProject;
 
 class ControllerHome extends Controller {
 
+    /**
+    * Method show($view_name).
+    * Invoke the method view.
+    * May perform logical operations and pass
+    * data to method view before invoking it.
+    *
+    * @param string $view_name The name of the view.
+    * @return function view($view_name, $data)
+    *   The method to be invoked.
+    */
     public static function show($view_name){
         self::view($view_name);
     }
