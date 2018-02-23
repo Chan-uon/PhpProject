1a5179a8b5d817737bee4c537032ca4b5fb7c2ff
Channy <Chan-uon@users.noreply.github.com>
2018-02-22 21:49:48 -0500

    Add a form to sign_up.php

--- views/sign_up.php
+++ views/sign_up.php
@@ -1,7 +1,3 @@
-<?php    
-    use \MyProject\PdoDb;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,5 +6,23 @@
     <body>
     <h1><?php echo $_GET['url']; ?> </h1>
     <a href="index.php">Home</a>
+    <br />
+    <form>
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" value="first name"><br />
+        
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" value="last name"><br />
+        
+        <label for="username">Username:</label>
+        <input type="text" id="username" value="username"><br />
+        
+        <label for="userpw">Password:</label>
+        <input type="password" id="userpw" value=""><br />
+        
+        <label for="userconfirm">Password confirm:</label>
+        <input type="password" id="userconfirm"value=""><br />
+        <input type="submit" value="submit">
+    </form>
     </body>
 </html>
